7637e3dbe090679c1047f361deac242876d4c769
carolchau <carol.chau@macaulay.cuny.edu>
2016-08-04 11:48:14 -0400

    organized files

---
+++ Dashboard/Data/alljsonDay.js
@@ -0,0 +1,14 @@
+/* All videos (~17k)
+Amount of videos watched per DoW */
+
+var jsonDAY = [
+    {
+        "Friday": 2265, 
+        "Monday": 2666, 
+        "Saturday": 2446, 
+        "Sunday": 2753, 
+        "Thursday": 2397, 
+        "Tuesday": 2598, 
+        "Wednesday": 2441
+    }
+];
