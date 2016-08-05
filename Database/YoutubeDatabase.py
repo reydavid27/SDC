1dd92d317738e4476007a78ff64f26858dcfdf70
reydavid86 <dav_8612@yahoo.com>
2016-08-05 11:45:21 -0400

    Fixed up the YoutubeDayScraper.  Mostly added comments.

--- Database/YoutubeDatabase.py
+++ Database/YoutubeDatabase.py
@@ -1,15 +1,23 @@
+#This Python script loads a text file containing JSON data and outputs it to a database
+#Programmers: David Acuqui & Carol Chau
+#Created for Social Data Collective
+
 import json
 import sqlite3 as lite
+import isodate
+
 
 data = ""
 value = []
-con = lite.connect('sdctest.db')
+con = lite.connect('SDCdatabase.db')
 
 with con:
     cur = con.cursor()
     cur.execute("DROP TABLE IF EXISTS Youtube")
-    cur.execute("CREATE TABLE Youtube(ID TEXT, Title TEXT)")
+    cur.execute("CREATE TABLE Youtube(ID TEXT, Title TEXT, VideoViewCount INT, VideoLength INT, "
+                "ChannelName TEXT, ChannelSubscriberCount INT, Categories TEXT, Tags TEXT)")
     value = []
+    data_dict = []
     with open("newData.txt", "r") as f:
         data = json.loads(f.read())
     for i in range(len(data)):
@@ -17,6 +25,32 @@ with con:
             value.append(key)
     for i in range(len(value)):
         youtube_id = value[i]
-        title = data[i][str(value[i])]["Title"]
-        cur.execute("INSERT INTO Youtube(ID, Title) VALUES(?, ?)", (youtube_id , title,))        
-    
+        title = data[i][value[i]]["Title"]
+        video_length = data[i][value[i]]["VideoLength"]
+        video_views = data[i][value[i]]["ViewCount"]
+        channel_name = data[i][value[i]]["ChannelName"]
+        channel_sub_count = data[i][value[i]]["SubCount"]
+        categories = data[i][value[i]]["Categories"]
+        tags = data[i][(value[i])]["Tags"]
+        #print tags
+
+        cur.execute("INSERT INTO Youtube(ID, Title, VideoViewCount, VideoLength, ChannelName, ChannelSubscriberCount, Categories) "
+            "VALUES (?,?,?,?,?,?,?)",
+            (youtube_id, title, video_views, video_length, channel_name, channel_sub_count, categories,))
+'''
+    for i in range(len(value)):
+        data_dict = [value[i],
+                    data[i][value[i]]["Title"],
+                    data[i][value[i]]["VideoLength"],
+                    data[i][value[i]]["ViewCount"],
+                    data[i][value[i]]["ChannelName"],
+                    data[i][value[i]]["SubCount"],
+                    data[i][value[i]]["Categories"]
+        data.append(zip(youtube_id, title, video_length, video_views, channel_name, channel_sub_count, categories))
+
+    print data
+    #cur.executemany("INSERT INTO Youtube(ID, Title, VideoViewCount, VideoLength, ChannelName, ChannelSubscriberCount, Categories) "
+    #    "VALUES (?,?,?,?,?,?,?)", data)
+
+
+'''
--- Python/YoutubeDayScraper.py
+++ Python/YoutubeDayScraper.py
@@ -3,7 +3,7 @@ import json
 import requests
 import datetime
 
-
+#These are the arrays and variables declared for us
 youtube_id = []
 already_stored = set()
 date = []
@@ -12,12 +12,14 @@ user_info = {}
 data = {}
 user_dict = []
 sdc_data = []
-keep_track_of_users = [[], [], [], [], [], [], []]
-Num_of_Vids_Watched = [0, 0, 0, 0, 0, 0, 0]
+keepTrackofUsers = [[], [], [], [], [], [], []]
+numofVidsWatched = [0, 0, 0, 0, 0, 0, 0]
 AvgforDay = [0,0,0,0,0,0,0]
 mon_count, tues_count, wed_count, thurs_count, fri_count, sat_count, sun_count = 0, 0, 0, 0, 0, 0, 0
 mon_total, tues_total, wed_total, thurs_total, fri_total, sat_total, sun_total = 0, 0, 0, 0, 0, 0, 0
 
+
+#These are used for the url building to make the API call
 base_url = "https://www.googleapis.com/youtube/v3/"
 video_url = base_url + "videos?part=snippet%2C+contentDetails%2C+statistics&id="
 rest_vid_url = "&fields=items(contentDetails(caption%2Cdefinition%2Cduration)%2Cetag%2Cid%2Ckind%2Csnippet(categoryId%2CchannelId%2CchannelTitle%2Ctags%2Ctitle)%2Cstatistics(dislikeCount%2CviewCount))%2Ckind&key="
@@ -35,8 +37,8 @@ def read_file():
             url_link = row['url']
             user_row = row['user_id']
             date_row = row['timestamp']
-            yt_id = url_link[32:]
-            date_fix = date_row[:10]
+            yt_id = url_link[32:] #fixing the url string to grab the Youtube ID
+            date_fix = date_row[:10] #fixing the date string to delete the time stamp
             if "youtube.com/watch" in url_link:
                 if len(yt_id) > 11:
                     pass
@@ -47,6 +49,7 @@ def read_file():
                         date.append(date_fix)
                         user_day(user_row, yt_id, date_fix)
     calculate_avg()
+    '''
     print "Mon count ", mon_count
     print "Tues count ", tues_count
     print "Wed count ", wed_count
@@ -62,7 +65,10 @@ def read_file():
     print "Fri total", fri_total
     print "Sat total", sat_total
     print "Sun total", sun_total
+    '''
 
+#A function used to convert the date into a weekday string
+#and appends the User ID and Weekday into a dictionary
 def user_day(x, y, z):
     full_url = video_url + y + "&key=" + youtube_key
     result = requests.get(full_url, verify=True)
@@ -80,9 +86,12 @@ def user_day(x, y, z):
             user_info[key].append(weekday)
         else:
             pass
-
-
-# x = key/User ID , y = date_together , z = weekday
+'''
+A function used to gather the info needed to calculate the average number of videos watched.
+It takes three parameters: x = key/User ID , y = date_together , z = weekday
+KeepTrackofUsers is an array of an array size 7 (for the days of the week)
+NumOfVidsWatched is an array for also keeping count of the days of the week
+'''
 def count_total_days(x, y, z):
     global mon_count, tues_count, wed_count, thurs_count, fri_count, sat_count, sun_count
     global mon_total, tues_total, wed_total, thurs_total, fri_total, sat_total, sun_total
@@ -90,45 +99,45 @@ def count_total_days(x, y, z):
 
     if z == "Monday":
         mon_count += 1
-        if user not in keep_track_of_users[0]:
-            keep_track_of_users[0].append(user)
-            Num_of_Vids_Watched[0] = Num_of_Vids_Watched[0] + 1
+        if user not in keepTrackofUsers[0]:
+            keepTrackofUsers[0].append(user)
+            numofVidsWatched[0] = numofVidsWatched[0] + 1
 
     elif z == "Tuesday":
         tues_count += 1
-        if user not in keep_track_of_users[1]:
-            keep_track_of_users[1].append(user)
-            Num_of_Vids_Watched[1] = Num_of_Vids_Watched[1] + 1
+        if user not in keepTrackofUsers[1]:
+            keepTrackofUsers[1].append(user)
+            numofVidsWatched[1] = numofVidsWatched[1] + 1
 
     elif z == "Wednesday":
         wed_count += 1
-        if user not in keep_track_of_users[2]:
-            keep_track_of_users[2].append(user)
-            Num_of_Vids_Watched[2] = Num_of_Vids_Watched[2] + 1
+        if user not in keepTrackofUsers[2]:
+            keepTrackofUsers[2].append(user)
+            numofVidsWatched[2] = numofVidsWatched[2] + 1
 
     elif z == "Thursday":
         thurs_count += 1
-        if user not in keep_track_of_users[3]:
-            keep_track_of_users[3].append(user)
-            Num_of_Vids_Watched[3] = Num_of_Vids_Watched[3] + 1
+        if user not in keepTrackofUsers[3]:
+            keepTrackofUsers[3].append(user)
+            numofVidsWatched[3] = numofVidsWatched[3] + 1
 
     elif z == "Friday":
         fri_count += 1
-        if user not in keep_track_of_users[4]:
-            keep_track_of_users[4].append(user)
-            Num_of_Vids_Watched[4] = Num_of_Vids_Watched[4] + 1
+        if user not in keepTrackofUsers[4]:
+            keepTrackofUsers[4].append(user)
+            numofVidsWatched[4] = numofVidsWatched[4] + 1
 
     elif z == "Saturday":
         sat_count += 1
-        if user not in keep_track_of_users[5]:
-            keep_track_of_users[5].append(user)
-            Num_of_Vids_Watched[5] = Num_of_Vids_Watched[5] + 1
+        if user not in keepTrackofUsers[5]:
+            keepTrackofUsers[5].append(user)
+            numofVidsWatched[5] = numofVidsWatched[5] + 1
 
     elif z == "Sunday":
         sun_count += 1
-        if user not in keep_track_of_users[6]:
-            keep_track_of_users[6].append(user)
-            Num_of_Vids_Watched[6] = Num_of_Vids_Watched[6] + 1
+        if user not in keepTrackofUsers[6]:
+            keepTrackofUsers[6].append(user)
+            numofVidsWatched[6] = numofVidsWatched[6] + 1
 
     if y not in already_stored:
         already_stored.add(y)
@@ -148,36 +157,39 @@ def count_total_days(x, y, z):
             sun_total += 1
     else:
         pass
-
+'''
+This function calculates the average using the info from
+the Count_Total_days function
+'''
 def calculate_avg():
-    if Num_of_Vids_Watched[0] == 0 or mon_total == 0:
+    if numofVidsWatched[0] == 0 or mon_total == 0:
         pass
     else:
-        AvgforDay[0] = (mon_count / (Num_of_Vids_Watched[0] * mon_total))
-    if Num_of_Vids_Watched[1] == 0 or tues_total == 0:
+        AvgforDay[0] = (mon_count / (numofVidsWatched[0] * mon_total))
+    if numofVidsWatched[1] == 0 or tues_total == 0:
         pass
     else:
-        AvgforDay[1] = (tues_count / (Num_of_Vids_Watched[1] * tues_total))
-    if Num_of_Vids_Watched[2] == 0 or wed_total == 0:
+        AvgforDay[1] = (tues_count / (numofVidsWatched[1] * tues_total))
+    if numofVidsWatched[2] == 0 or wed_total == 0:
         pass
     else:
-        AvgforDay[2] = (wed_count / (Num_of_Vids_Watched[2] * wed_total))
-    if Num_of_Vids_Watched[3] == 0 or thurs_total == 0:
+        AvgforDay[2] = (wed_count / (numofVidsWatched[2] * wed_total))
+    if numofVidsWatched[3] == 0 or thurs_total == 0:
         pass
     else:
-        AvgforDay[3] = (thurs_count / (Num_of_Vids_Watched[3] * thurs_total))
-    if Num_of_Vids_Watched[4] == 0 or fri_total == 0:
+        AvgforDay[3] = (thurs_count / (numofVidsWatched[3] * thurs_total))
+    if numofVidsWatched[4] == 0 or fri_total == 0:
         pass
     else:
-        AvgforDay[4] = (fri_count / (Num_of_Vids_Watched[4] * fri_total))
-    if Num_of_Vids_Watched[5] == 0 or sat_total == 0:
+        AvgforDay[4] = (fri_count / (numofVidsWatched[4] * fri_total))
+    if numofVidsWatched[5] == 0 or sat_total == 0:
         pass
     else:
-        AvgforDay[5] = (sat_count / (Num_of_Vids_Watched[5] * sat_total))
-    if Num_of_Vids_Watched[6] == 0 or sun_total == 0:
+        AvgforDay[5] = (sat_count / (numofVidsWatched[5] * sat_total))
+    if numofVidsWatched[6] == 0 or sun_total == 0:
         pass
     else:
-        AvgforDay[6] = (tues_count / (Num_of_Vids_Watched[6] * sun_total))
+        AvgforDay[6] = (tues_count / (numofVidsWatched[6] * sun_total))
         
     '''    
     print 'avg m: ', AvgforDay[0]
@@ -188,7 +200,10 @@ def calculate_avg():
     print 'avg s:', AvgforDay[5]
     print 'avg sn:', AvgforDay[6]
     '''
-
+'''
+Function to count the number of a videos an individual user watches.
+It also calculates the average of this total.
+'''
 def count_days():
     read_file()
     global mon_count, tues_count, wed_count, thurs_count, fri_count, sat_count, sun_count
@@ -232,8 +247,8 @@ def count_days():
         
         mon_count, tues_count, wed_count, thurs_count, fri_count, sat_count, sun_count = 0,0,0,0,0,0,0
 
+#Function to output the data into a json text file
 def json_file():
-    #read_file()
     count_days()
 
     for i in range(len(user_dict)):
@@ -268,9 +283,7 @@ def json_file():
         json.dump(sdc_data2, n, indent= 4)
 
 if __name__ == '__main__':
-    #count_days()
     json_file()
-    #read_file()
     print "success"
 
 
